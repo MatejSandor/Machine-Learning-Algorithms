02a3b200c44c7be2ef19872af8f7631a84834282
Matej Šándor <matejko.sandor@gmail.com>
2020-06-22 14:50:08 +0200

    add logistic regression basic structure

---
+++ Logistic_Regression/LogisticRegression.py
@@ -0,0 +1,18 @@
+import numpy
+
+
+class LogisticRegression:
+
+    def __init__(self, lr=0.001, n_iterations=1000):
+        self.lr = lr
+        self.n_iterations = n_iterations
+        self.weights = None
+        self.bias = None
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+
