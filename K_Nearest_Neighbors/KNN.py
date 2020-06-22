997227377ef89e1eb557987f2d5d9233941885ca
Matej Šándor <matejko.sandor@gmail.com>
2020-06-22 12:39:46 +0200

    add complete KNN implementation

--- K_Nearest_Neighbors/KNN.py
+++ K_Nearest_Neighbors/KNN.py
@@ -1,3 +1,9 @@
+import numpy
+from collections import Counter
+
+
+def euclidean_distance(x1, x2):
+    return numpy.sqrt(numpy.sum((x1-x2)**2))
 
 
 class KNN:
@@ -5,7 +11,17 @@ class KNN:
         self.k = k
 
     def fit(self, x, y):
-        pass
+        self.x_train = x
+        self.y_train = y
 
     def predict(self, x):
-        pass
+        predicted_labels = [self._predict(element) for element in x]
+        return numpy.array(predicted_labels)
+
+    def _predict(self, x):
+        distances = [euclidean_distance(x, train_element) for train_element in self.x_train]
+        k_indices = numpy.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
+
--- K_Nearest_Neighbors/KNN_test.py
+++ K_Nearest_Neighbors/KNN_test.py
@@ -1,16 +1,31 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import numpy
 from matplotlib.colors import ListedColormap
+
+from K_Nearest_Neighbors.KNN import KNN
 
 color_map = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+def accuracy(y_true, y_pred):
+    accuracy_measurement = numpy.sum(y_true == y_pred) / len(y_true)
+    return accuracy_measurement
+
+
+iris = datasets.load_iris()
+x, y = iris.data, iris.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
 
 plt.figure()
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=color_map, edgecolor='k', s=20)
+plt.scatter(x[:, 0], x[:, 1], c=y, cmap=color_map, edgecolor='k', s=20)
 plt.show()
 
+k = 3
+clf = KNN(k=k)
+clf.fit(x_train, y_train)
+predictions = clf.predict(x_test)
+
+print("custom KNN classification accuracy", accuracy(y_test, predictions))
