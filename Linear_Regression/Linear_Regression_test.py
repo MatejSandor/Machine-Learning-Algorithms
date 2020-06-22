95eb57cc9c565c06fc01bdcf787bbc216efb0e5e
Matej Šándor <matejko.sandor@gmail.com>
2020-06-22 14:40:17 +0200

    add linear regression visualization

--- Linear_Regression/Linear_Regression_test.py
+++ Linear_Regression/Linear_Regression_test.py
@@ -3,10 +3,27 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
 
+from Linear_Regression.LinearRegression import LinearRegression
+
+
+def mean_squared_error(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
 
 x, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1234)
 
+regression = LinearRegression(lr=0.01)
+regression.fit(x_train, y_train)
+predicted = regression.predict(x_test)
+
+mse = mean_squared_error(y_test, predicted)
+print(mse)
+
+y_pred_line = regression.predict(x)
+color_map = plt.get_cmap('viridis')
 fig = plt.figure(figsize=(8, 6))
-plt.scatter(x[:, 0], y, color="b", marker="o", s=30)
+m1 = plt.scatter(x_train, y_train, color=color_map(0.9), s=10)
+m2 = plt.scatter(x_test, y_test, color=color_map(0.5), s=10)
+plt.plot(x, y_pred_line, color='black', linewidth=2, label="Prediction")
 plt.show()
